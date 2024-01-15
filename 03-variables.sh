f7ad1bc2b9577f36781967d996a9612b37dde9da
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-01-16 03:00:12 +0530

    shell script

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please enter username:"
+
+read USERNAME
+
+echo "please enter the password"
+
+read PASSWORD
+
+echo "your user name and password is $USERNAME and $PASSWORD"
+
